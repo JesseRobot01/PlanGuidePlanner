11454e2a7f9bc1cada3364c46bf2fb32073cefb8
Jesse Knaapen <jesse.knaapen@outlook.com>
2024-01-19 22:39:15 +0100

    Log to file

--- StudyGuide/Application.cpp
+++ StudyGuide/Application.cpp
@@ -4,6 +4,7 @@
 
 #include "Application.h"
 
+#include <iostream>
 #include <thread>
 #include <QProcess>
 #include <QCommandLineParser>
@@ -13,10 +14,25 @@
 #include "ui/guide/Guide.h"
 
 #ifdef WIN32
+
 #include "WindowConsole.h"
+
 #endif
 
-Application::Application(int&argc, char** argv) : QApplication(argc, argv) {
+void messageHandler(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+    static std::mutex loggerMutex;
+    const std::lock_guard<std::mutex> lock(loggerMutex); // synchronized, QFile logFile is not thread-safe
+
+    QString out = qFormatLogMessage(type, context, msg);
+    out += QChar::LineFeed;
+
+    QTextStream(stderr) << out.toLocal8Bit();
+    APPLICATION->logFile->write(out.toUtf8());
+    APPLICATION->logFile->flush();
+    fflush(stderr);
+}
+
+Application::Application(int &argc, char **argv) : QApplication(argc, argv) {
 #ifdef WIN32
     AttachWindowsConsole();
 #endif
@@ -27,8 +43,8 @@ Application::Application(int&argc, char** argv) : QApplication(argc, argv) {
     QCommandLineParser commandLineParser;
 
     commandLineParser.addOptions({
-            {{"L", "lang"}, "Sets the language of the application", "lang"}
-        }
+                                         {{"L", "lang"}, "Sets the language of the application", "lang"}
+                                 }
     );
     commandLineParser.addHelpOption();
 
@@ -36,23 +52,37 @@ Application::Application(int&argc, char** argv) : QApplication(argc, argv) {
 
     commandLineParser.process(arguments());
 
+    QSettings settings;
+
     qSetMessagePattern(
-        "%{time process}"
-        " "
-        "%{if-debug}DEBUG   %{endif}"
-        "%{if-info}INFO    %{endif}"
-        "%{if-warning}WARNING %{endif}"
-        "%{if-critical}CRITICAL%{endif}"
-        "%{if-fatal}FATAL   %{endif}"
-        " "
-        "|"
-        " "
-        "%{message}");
+            "%{time process}"
+            " "
+            "%{if-debug}DEBUG   %{endif}"
+            "%{if-info}INFO    %{endif}"
+            "%{if-warning}WARNING %{endif}"
+            "%{if-critical}CRITICAL%{endif}"
+            "%{if-fatal}FATAL   %{endif}"
+            " "
+            "|"
+            " "
+            "%{message}");
+
+    QDir logsDir(settings.value("logDir", QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) +
+                                          "/logs").toString());
+    if (!logsDir.exists())
+        logsDir.mkpath(".");
+
+    // get highest number.
+    int latestLog = logsDir.entryList(QDir::Files).count();
+
+    logFile = std::unique_ptr<QFile>(new QFile(logsDir.path() + "/Log" + QString::number(latestLog + 1) + ".txt"));
+    if (!logFile->open(QIODevice::WriteOnly | QIODevice::Text | QIODevice::Truncate))
+        qFatal() << "Can't open log file!";
+
+    qInstallMessageHandler(messageHandler);
 
     translator = new QTranslator();
 
-
-    QSettings settings;
 
     if (!commandLineParser.value("lang").isEmpty()) {
         settings.setValue("Lang", commandLineParser.value("lang"));
@@ -80,10 +110,10 @@ Application::Application(int&argc, char** argv) : QApplication(argc, argv) {
         if (autoOpenDir.exists() && !autoOpenDir.isEmpty()) {
             QStringList guideFileNames = autoOpenDir.entryList(QDir::Files);
             QStringList guideFiles;
-            LoadGuide* loadGuide = new LoadGuide(nullptr, guideFileNames.count() * 2); // 1 for reading, 1 for opening.
+            LoadGuide *loadGuide = new LoadGuide(nullptr, guideFileNames.count() * 2); // 1 for reading, 1 for opening.
             loadGuide->show();
 
-            for (const QString&GuideFileName: guideFileNames)
+            for (const QString &GuideFileName: guideFileNames)
                 guideFiles.append(autoOpenDir.filePath(GuideFileName));
 
             QVector<GuideData::Data> guides = XmlParser::readXml(guideFiles);
@@ -101,19 +131,17 @@ Application::Application(int&argc, char** argv) : QApplication(argc, argv) {
 Application::~Application() {
 }
 
-void Application::setLanguage(const QString&languageCode) {
+void Application::setLanguage(const QString &languageCode) {
     if (translator->load(":/translations/StudyGuide_" + languageCode + ".qm")) {
         installTranslator(translator);
         qDebug() << "Succesfully loaded translations for" << languageCode;
-    }
-    else {
+    } else {
         qCritical() << "Failed to load translation for" << languageCode;
         qInfo() << "Falling back to default translations.";
 
         if (translator->load(":/translations/StudyGuide_en.qm")) {
             installTranslator(translator);
-        }
-        else {
+        } else {
             qCritical() << "Failed to load translations.";
         }
     }
--- StudyGuide/Application.h
+++ StudyGuide/Application.h
@@ -8,24 +8,28 @@
 #define APPLICATION (static_cast<Application*>(QCoreApplication::instance()))
 
 
+#include <QFile>
+
 #include "guide/GuideData.h"
 #include "ui/MainWindow.h"
 
-class Application : public QApplication
-{
+class Application : public QApplication {
 public:
-    Application(int& argc, char** argv);
+    Application(int &argc, char **argv);
 
     virtual ~Application();
 
     static void restart();
 
+    std::unique_ptr<QFile> logFile;
 
 public slots:
+
     static void addGuide(GuideData::Data data);
 
 private:
-    void setLanguage(const QString& languageCode);
-     MainWindow* appWindow;
-     QTranslator* translator;
+    void setLanguage(const QString &languageCode);
+
+    MainWindow *appWindow;
+    QTranslator *translator;
 };
