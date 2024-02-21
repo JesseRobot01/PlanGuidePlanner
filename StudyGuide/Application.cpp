fd471a4c19134b5502c57483a48eb4e044754942
Jesse <Jesse.Knaapen@outlook.com>
2024-02-21 13:41:18 +0100

    Merge didn't merged.

--- StudyGuide/Application.cpp
+++ StudyGuide/Application.cpp
@@ -96,40 +96,12 @@ Application::Application(int&argc, char** argv) : QApplication(argc, argv) {
         QString currentTheme = settings.value("Theme", "fusion_dark").toString();
 
         if (currentTheme == "fusion_dark") {
-            setStyle("fusion");
-            QPalette darkPalette;
-            darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
-            darkPalette.setColor(QPalette::WindowText, Qt::white);
-            darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
-            darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-            darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-            darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-            darkPalette.setColor(QPalette::Text, Qt::white);
-            darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
-            darkPalette.setColor(QPalette::ButtonText, Qt::white);
-            darkPalette.setColor(QPalette::BrightText, Qt::red);
-            darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-            darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-            darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-            setPalette(darkPalette);
+           GuidePalette palette;
+           palette.setFusionDark();
         }
         if (currentTheme == "fusion_light") {
-            setStyle("fusion");
-            QPalette lightPalette;
-            lightPalette.setColor(QPalette::Window, Qt::white);
-            lightPalette.setColor(QPalette::WindowText, Qt::black);
-            lightPalette.setColor(QPalette::Base, Qt::white);
-            lightPalette.setColor(QPalette::AlternateBase, QColor(240, 240, 240));
-            lightPalette.setColor(QPalette::ToolTipBase, Qt::white);
-            lightPalette.setColor(QPalette::ToolTipText, Qt::black);
-            lightPalette.setColor(QPalette::Text, Qt::black);
-            lightPalette.setColor(QPalette::Button, QColor(240, 240, 240));
-            lightPalette.setColor(QPalette::ButtonText, Qt::black);
-            lightPalette.setColor(QPalette::BrightText, Qt::red);
-            lightPalette.setColor(QPalette::Link, QColor(42, 130, 218));
-            lightPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
-            lightPalette.setColor(QPalette::HighlightedText, Qt::white);
-            setPalette(lightPalette);
+            GuidePalette palette;
+            palette.setFusionLight();
         }
         if (currentTheme == "system") {
             // let system decide. So no passing in agruments.
