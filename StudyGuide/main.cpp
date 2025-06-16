35d249441afe0533ed670b2df3fd2a928b37d153
Jesse Knaapen <Jesse.Knaapen@outlook.com>
2025-06-16 16:40:21 +0200

    Empty junk and add _headers

--- StudyGuide/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "ui/MainWindow.h"
-#include "Application.h"
-
-// smallest main.cpp file ever!
-int main(int argc, char* argv[]) {
-    Application app(argc, argv);
-    return app.exec();
-}
